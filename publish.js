9bcffe2b37700f8c93065690f3848322a73485bb
Alex M <milesAwayAlex@gmail.com>
2021-12-21 15:48:37 -0800

    Implement publish/subscribe functionality, broadcast messages

---
+++ publish.js
@@ -0,0 +1,19 @@
+const amqp = require('amqplib/callback_api');
+const msg = process.argv.slice(2).join(' ') || 'Hello World!';
+const exchange = 'logs';
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) throw error0;
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1;
+
+    channel.assertExchange(exchange, 'fanout', { durable: false });
+
+    channel.publish(exchange, '', Buffer.from(msg));
+    console.log(' [x] Sent', msg);
+  });
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 666);
+});
